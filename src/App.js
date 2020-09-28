869e5746554d578d74d8d7840ddc4800fc8762ca
Iyad <iyadhb@gmail.com>
2020-09-28 22:58:46 +0100

    changed to class component with state

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import React from 'react';
 import TodoItem from './components/TodoItem';
 import todosData from './todosData';
 
-function App() {
-  const taskItemComponents = todosData.map((item) => <TodoItem key={item.id} task={item} />);
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData,
+    };
+  }
+  render() {
+    const taskItemComponents = this.state.todos.map((item) => (
+      <TodoItem key={item.id} task={item} />
+    ));
 
-  return <div className='todo-list'>{taskItemComponents}</div>;
+    return <div className='todo-list'>{taskItemComponents}</div>;
+  }
 }
 
 export default App;
