125296d885e0782cedaeb75344f3fb8a21ada1d0
Iyad <iyadhb@gmail.com>
2020-09-28 21:50:37 +0100

    passing tasks from an array

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import TodoItem from './components/TodoItem';
+import todosData from './todosData';
 
 function App() {
-  return (
-    <div className='todo-list'>
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
-    </div>
-  );
+  const taskItemComponents = todosData.map((item) => <TodoItem key={item.id} task={item} />);
+
+  return <div className='todo-list'>{taskItemComponents}</div>;
 }
 
 export default App;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const TodoItem = () => {
+const TodoItem = (props) => {
   return (
     <div className='todo-item'>
-      <input type='checkbox' />
-      <p>Placeholder text here</p>
+      <input type='checkbox' checked={props.task.completed} />
+      <p>{props.task.text}</p>
     </div>
   );
 };
