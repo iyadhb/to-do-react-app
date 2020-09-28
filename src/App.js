71918ac5e480d90af6cd4fb8ddb2d0f9d5d0ebe1
Iyad <iyadhb@gmail.com>
2020-09-28 21:28:08 +0100

    first phase of project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import './App.css';
+import TodoItem from './components/TodoItem';
 
 function App() {
   return (
-    <div>
-      <h1>My TO Do App</h1>
+    <div className='todo-list'>
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
     </div>
   );
 }
